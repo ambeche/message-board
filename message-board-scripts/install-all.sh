57ba186d2ae20996c5d67a5c89a2cb5db68f2341
tamanjic <tamanji.che@metropolia.fi>
2024-01-21 23:03:48 +0200

    chore: Completed project set up.

---
+++ message-board-scripts/install-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+BLUE="\033[0;34m"
+BBLUE="\033[1;34m"
+UBLUE="\033[4;34m"
+PURPLE="\033[0;35m"
+BPURPLE="\033[1;35m"
+UPURPLE="\033[4;35m"
+NC='\033[0m' # No Color
+
+backend="./server"
+frontend="../react-client"
+
+cd ${backend} &&
+  echo "----------------" &&
+  echo -e "${PURPLE}Installing Backend dependencies...${NC}" &&
+  echo "----------------" &&
+  npm install && cd ${frontend} &&
+  echo -e "${PURPLE}Now installing Frontend dependencies...${NC}" &&
+  npm install &&
+  echo -e "----------------" &&
+  echo -e "${GREEN}Installations completed successfully!${NC}"
+echo -e "           "
+echo -e "${UPURPLE} You can now run both the react-client and the server of ${UBLUE}Message-Board app ${UPURPLE}in a single command:${NC}"
+echo -e "           "
+echo -e " =>  ${BBLUE}npm start ${NC}"
---
+++ message-board-scripts/start-server-and-client.bat
@@ -0,0 +1,18 @@
+
+@echo off
+
+:: Setting variables
+set "backend=../server"
+set "frontend=../react-client"
+set "serverTitle=Backend Console"
+set "clientTitle=Frontend Console"
+
+echo Starting Backend and Frontend of Message-Board app in 2 separate CMD terminals...
+
+
+:: Starting the server with a custom title
+start cmd.exe /K "title %serverTitle% &&  echo %serverTitle% && cd %backend% && npm run dev"
+
+:: Starting the client with a custom title
+start cmd.exe /K "title %clientTitle% && echo %clientTitle% && cd %frontend% && npm run dev"
+
