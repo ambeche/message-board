0ebcfd90a700da1d201709a97b3ddfb0548f6924
tamanjic <tamanji.che@metropolia.fi>
2024-02-11 18:38:14 +0200

    fix: Resolved type mismatch app theme interface.

--- react-client/src/types/actionTypes.ts
+++ react-client/src/types/actionTypes.ts
@@ -1,4 +1,4 @@
-import { Channel, Message } from './stateTypes';
+import { AppTheme, Channel, Message } from './stateTypes';
 
 export type SetMessagePayload = { messages: Message[]; channelId: string };
 export type MessageBoardAction =
@@ -6,4 +6,4 @@ export type MessageBoardAction =
   | { type: 'SELECT_CHANNEL'; payload: Channel }
   | { type: 'SET_MESSAGES'; payload: SetMessagePayload }
   | { type: 'ADD_MESSAGE'; payload: { message: Message; channelId: string } }
-  | { type: 'SET_THEME'; payload: string };
+  | { type: 'SET_THEME'; payload: AppTheme };
