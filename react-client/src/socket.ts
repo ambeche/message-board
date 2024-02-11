175385da61cfb54ae7a473dfe2d4875d17717b20
tamanjic <tamanji.che@metropolia.fi>
2024-02-11 18:04:16 +0200

    refactor: removed unused codes.

--- react-client/src/socket.ts
+++ react-client/src/socket.ts
@@ -4,6 +4,5 @@ const URI =
   process.env.NODE_ENV === 'production'
     ? 'https://messenge--board-64c5f8903fd9.herokuapp.com/'
     : 'http://localhost:3005';
-console.log('env: ', process.env.NODE_ENV);
 
 export const socket = io(URI);
