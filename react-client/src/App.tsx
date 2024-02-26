83c7e9ac6ae6397c5cd389bf7bbf05f76a25d240
tamanjic <tamanji.che@metropolia.fi>
2024-02-26 20:56:32 +0200

    feat: Implemented paging for mobile layout of the app and tweaked the UI to improve intuitiveness.

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -4,21 +4,56 @@ import MessageList from './components/MessageList/MessageList';
 import { useMessageBoard } from './context/useMessageBoard';
 import './App.css';
 import { toggledTheme } from './utils';
+import { useState, useEffect } from 'react';
+
+const useViewport = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  return { width };
+};
 
 const App = () => {
   const { selectedChannel, theme } = useMessageBoard();
+  const { width } = useViewport();
+  const [mobileView] = useState('channelListView');
   return (
     <div className={`app-container ${toggledTheme(theme)}`}>
-      <ChannelList />
-      <div className='message-container'>
-        <div className='message-header'>
-          <div>Message Board</div>
-          <div className='selected-channel'>{selectedChannel?.id}</div>
-        </div>
+      {width < 768 ? (
+        <>
+          {mobileView === 'channelListView' && !selectedChannel?.id ? (
+            <ChannelList />
+          ) : (
+            <div className='message-container'>
+              <div className='message-header'>
+                <div>Message Board</div>
+                <div className='selected-channel'>{selectedChannel?.id}</div>
+              </div>
 
-        <MessageList />
-        <MessageEditor key={selectedChannel?.id} />
-      </div>
+              <MessageList />
+              <MessageEditor key={selectedChannel?.id} />
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <ChannelList />
+          <div className='message-container'>
+            <div className='message-header'>
+              <div>Message Board</div>
+              <div className='selected-channel'>{selectedChannel?.id}</div>
+            </div>
+
+            <MessageList />
+            <MessageEditor key={selectedChannel?.id} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
